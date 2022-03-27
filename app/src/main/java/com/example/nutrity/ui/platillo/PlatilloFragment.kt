3ffdc27b5a0aded20bb0ed24ed3511e9b5cdb0f9
Ivan-Urenda <81488477+Ivan-Urenda@users.noreply.github.com>
2022-03-27 14:16:02 -0600

    Acelerometro y agregar imagen de perfil

--- app/src/main/java/com/example/nutrity/ui/platillo/PlatilloFragment.kt
+++ app/src/main/java/com/example/nutrity/ui/platillo/PlatilloFragment.kt
@@ -1,14 +1,24 @@
 package com.example.nutrity.ui.platillo
 
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import com.example.nutrity.R
 
-class PlatilloFragment : Fragment() {
+class PlatilloFragment : Fragment(), SensorEventListener {
+
+    var sensorManager: SensorManager?= null
+    var sensor: Sensor?= null
+    private lateinit var recetaImage: ImageView
 
     companion object {
         fun newInstance() = PlatilloFragment()
@@ -20,13 +30,44 @@ class PlatilloFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.platillo_fragment, container, false)
+        val root = inflater.inflate(R.layout.platillo_fragment, container, false)
+        recetaImage = root.findViewById(R.id.platilloImage)
+
+        sensorManager = context?.getSystemService(AppCompatActivity.SENSOR_SERVICE) as SensorManager
+        sensor = sensorManager!!.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+
+
+        return root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(PlatilloViewModel::class.java)
         // TODO: Use the ViewModel
+    }
+
+    override fun onResume() {
+        super.onResume()
+        sensorManager!!.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (event?.sensor?.type == Sensor.TYPE_ACCELEROMETER){
+            val sides = event.values[0]
+            val upDown = event.values[1]
+
+            recetaImage.apply {
+                rotationX = upDown * 3f
+                rotationY = sides * 3f
+                rotation = -sides
+                translationX = sides * -10
+                translationY = upDown * 10
+            }
+        }
+    }
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+        return
     }
 
 }
--- app/src/main/java/com/example/nutrity/ui/signup/SignupActivity.kt
+++ app/src/main/java/com/example/nutrity/ui/signup/SignupActivity.kt
@@ -1,16 +1,23 @@
 package com.example.nutrity.ui.signup
 
+import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
-import android.widget.Toolbar
+import android.widget.*
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import com.example.nutrity.databinding.ActivitySignupBinding
 import com.example.nutrity.ui.login.LoginActivity
 
 class SignupActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySignupBinding
+    private lateinit var imagePerfil: ImageButton
+    private lateinit var register: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +27,68 @@ class SignupActivity : AppCompatActivity() {
         binding = ActivitySignupBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        imagePerfil = binding.imagePerfil
+        register = binding.register
+
+        imagePerfil.setOnClickListener { requestPermissions() }
+
+        register.setOnClickListener {
+            finish()
+        }
+
+
+
     }
+
+    private fun requestPermissions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+        {
+            when {
+                ContextCompat.checkSelfPermission(this,
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE
+                ) == PackageManager.PERMISSION_GRANTED -> {
+                    pickPhotoFromGallery()
+                }
+
+                else -> requestPermissionLauncher.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+            }
+        }
+        else
+        {
+            pickPhotoFromGallery()
+        }
+    }
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ){isGranted->
+        if (isGranted)
+        {
+            pickPhotoFromGallery()
+        }
+        else
+        {
+            Toast.makeText(this, "Se necesitan habilitar los permisos", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private val startForActivityGallery = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ){result ->
+
+        if (result.resultCode == Activity.RESULT_OK)
+        {
+            val data = result.data?.data
+            imagePerfil.setImageURI(data)
+        }
+    }
+
+    private fun pickPhotoFromGallery() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "image/*"
+        startForActivityGallery.launch(intent)
+    }
+
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
 
