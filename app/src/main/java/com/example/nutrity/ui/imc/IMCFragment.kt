3fe635a4cae7dd4b3255fb211b2fdbbda5162357
Ivan-Urenda <81488477+Ivan-Urenda@users.noreply.github.com>
2022-04-15 02:53:01 -0500

    Calculadora de calorias

--- app/src/main/java/com/example/nutrity/ui/imc/IMCFragment.kt
+++ app/src/main/java/com/example/nutrity/ui/imc/IMCFragment.kt
@@ -11,6 +11,7 @@ import android.widget.Toast
 import com.example.nutrity.R
 import com.example.nutrity.databinding.IMCFragmentBinding
 import kotlin.math.pow
+import kotlin.math.roundToInt
 
 class IMCFragment : Fragment() {
 
@@ -32,36 +33,202 @@ class IMCFragment : Fragment() {
         val root: View = binding.root
 
         val appContext = requireContext().applicationContext
-        val spinner = binding.spinner
-        val lista = listOf("Bajar de peso", "Subir de peso", "Mantener peso")
-        val adaptador = ArrayAdapter(appContext, R.layout.spinner_items, lista)
-        spinner.adapter = adaptador
+        val objetivo = binding.objetivo
+        val objetivos = listOf("Lose weight", "Gain weight", "Maintain weight")
+        var adaptador = ArrayAdapter(appContext, R.layout.spinner_items, objetivos)
+        objetivo.adapter = adaptador
+
+        val genero = binding.genero
+        val generos = listOf("Men", "Women")
+        adaptador = ArrayAdapter(appContext, R.layout.spinner_items, generos)
+        genero.adapter = adaptador
+
+        val fActividad = binding.fActividad
+        val fActividades = listOf("Sedentary", "Little activity", "Moderate activity", "Intense activity")
+        adaptador = ArrayAdapter(appContext, R.layout.spinner_items, fActividades)
+        fActividad.adapter = adaptador
 
 
         binding.calculateButton.setOnClickListener {
 
-            if(binding.weight.text.toString().isEmpty() || binding.height.text.toString().isEmpty()){
-                Toast.makeText(context, "Ingresa un valor valido", Toast.LENGTH_SHORT).show()
-            }
-            else if(binding.height.text.toString().toDouble() > 272){
-                Toast.makeText(context, "Ingresa una altura valido", Toast.LENGTH_SHORT).show()
+            if(binding.genero.selectedItem.toString() == "Men"){
+                if(binding.weight.text.toString().isEmpty() || binding.height.text.toString().isEmpty() || binding.age.toString().isEmpty()){
+                    Toast.makeText(context, "Please enter a valid value", Toast.LENGTH_SHORT).show()
+                }
+                else if(binding.height.text.toString().toDouble() > 272){
+                    Toast.makeText(context, "Please enter a valid height", Toast.LENGTH_SHORT).show()
+                }else if(binding.age.text.toString().toDouble() > 100){
+                    Toast.makeText(context, "Please enter a valid age", Toast.LENGTH_SHORT).show()
+                }
+                else{
+                    if(binding.fActividad.selectedItem.toString() == "Sedentary")
+                    {
+                        var cal = ((66 + (13.7 * binding.weight.text.toString().toDouble()) +
+                                ((5 * binding.height.text.toString().toDouble()) - (6.8 * binding.age.text.toString().toDouble()))) * 1.2).roundToInt()
+
+                        if(binding.objetivo.selectedItem.toString() == "Lose weight")
+                        {
+                            binding.result.text = "You must consume less than "+cal+" calories to lose weight"
+
+                        }else if(binding.objetivo.selectedItem.toString() == "Gain weight"){
+
+                            binding.result.text = "You must consume more than "+cal+" calories to gain weight"
+
+                        }
+                        else
+                        {
+                            binding.result.text = "You must consume "+cal+" calories to maintain your weight"
+                        }
+
+                    }else if(binding.fActividad.selectedItem.toString() == "Little activity"){
+
+                        var cal = ((66 + (13.7 * binding.weight.text.toString().toDouble()) +
+                                ((5 * binding.height.text.toString().toDouble()) - (6.8 * binding.age.text.toString().toDouble()))) * 1.375).roundToInt()
+
+                        if(binding.objetivo.selectedItem.toString() == "Lose weight")
+                        {
+                            binding.result.text = "You must consume less than "+cal+" calories to lose weight"
+
+                        }else if(binding.objetivo.selectedItem.toString() == "Gain weight"){
+
+                            binding.result.text = "You must consume more than "+cal+" calories to gain weight"
+
+                        }
+                        else
+                        {
+                            binding.result.text = "You must consume "+cal+" calories to maintain your weight"
+                        }
+
+                    }else if(binding.fActividad.selectedItem.toString() == "Moderate activity"){
+
+                        var cal = ((66 + (13.7 * binding.weight.text.toString().toDouble()) +
+                                ((5 * binding.height.text.toString().toDouble()) - (6.8 * binding.age.text.toString().toDouble()))) * 1.55).roundToInt()
+
+                        if(binding.objetivo.selectedItem.toString() == "Lose weight")
+                        {
+                            binding.result.text = "You must consume less than "+cal+" calories to lose weight"
+
+                        }else if(binding.objetivo.selectedItem.toString() == "Gain weight"){
+
+                            binding.result.text = "You must consume more than "+cal+" calories to gain weight"
+
+                        }
+                        else
+                        {
+                            binding.result.text = "You must consume "+cal+" calories to maintain your weight"
+                        }
+
+                    }
+                    else
+                    {
+                        var cal = ((66 + (13.7 * binding.weight.text.toString().toDouble()) +
+                                ((5 * binding.height.text.toString().toDouble()) - (6.8 * binding.age.text.toString().toDouble()))) * 1.725).roundToInt()
+
+                        if(binding.objetivo.selectedItem.toString() == "Lose weight")
+                        {
+                            binding.result.text = "You must consume less than "+cal+" calories to lose weight"
+
+                        }else if(binding.objetivo.selectedItem.toString() == "Gain weight"){
+
+                            binding.result.text = "You must consume more than "+cal+" calories to gain weight"
+
+                        }
+                        else
+                        {
+                            binding.result.text = "You must consume "+cal+" calories to maintain your weight"
+                        }
+
+                    }
+                }
             }
             else{
-                val weightValue = binding.weight.text.toString().toDouble()
-                val heightValue = binding.height.text.toString().toDouble() / 100
+                if(binding.weight.text.toString().isEmpty() || binding.height.text.toString().isEmpty() || binding.age.toString().isEmpty()){
+                    Toast.makeText(context, "Please enter a valid value", Toast.LENGTH_SHORT).show()
+                }
+                else if(binding.height.text.toString().toDouble() > 272){
+                    Toast.makeText(context, "Please enter a valid height", Toast.LENGTH_SHORT).show()
+                }else if(binding.age.text.toString().toDouble() > 100){
+                    Toast.makeText(context, "Please enter a valid age", Toast.LENGTH_SHORT).show()
+                }
+                else{
+                    if(binding.fActividad.selectedItem.toString() == "Sedentary")
+                    {
+                        var cal = ((655 + (9.6 * binding.weight.text.toString().toDouble()) +
+                                ((1.8 * binding.height.text.toString().toDouble()) - (4.7 * binding.age.text.toString().toDouble()))) * 1.2).roundToInt()
 
-                val imc = weightValue / heightValue.pow(2)
+                        if(binding.objetivo.selectedItem.toString() == "Lose weight")
+                        {
+                            binding.result.text = "You must consume less than "+cal+" calories to lose weight"
 
-                if (imc < 16) {
-                    binding.result.text = "IMC: " + imc + "\n\nEstado: Delgadez severa"
-                } else if (imc < 18.5) {
-                    binding.result.text = "IMC: " + imc + "\n\nEstado: Bajo peso"
-                } else if (imc in 18.5..24.9) {
-                    binding.result.text = "IMC: " + imc + "\n\nEstado: Normal"
-                } else if (imc >= 25) {
-                    binding.result.text = "IMC: " + imc + "\n\nEstado: Sobrepeso"
-                } else {
-                    binding.result.text = "IMC: " + imc + "\n\nEstado: Obesidad"
+                        }else if(binding.objetivo.selectedItem.toString() == "Gain weight"){
+
+                            binding.result.text = "You must consume more than "+cal+" calories to gain weight"
+
+                        }
+                        else
+                        {
+                            binding.result.text = "You must consume "+cal+" calories to maintain your weight"
+                        }
+
+                    }else if(binding.fActividad.selectedItem.toString() == "Little activity"){
+
+                        var cal = ((655 + (9.6 * binding.weight.text.toString().toDouble()) +
+                                ((1.8 * binding.height.text.toString().toDouble()) - (4.7 * binding.age.text.toString().toDouble()))) * 1.375).roundToInt()
+
+                        if(binding.objetivo.selectedItem.toString() == "Lose weight")
+                        {
+                            binding.result.text = "You must consume less than "+cal+" calories to lose weight"
+
+                        }else if(binding.objetivo.selectedItem.toString() == "Gain weight"){
+
+                            binding.result.text = "You must consume more than "+cal+" calories to gain weight"
+
+                        }
+                        else
+                        {
+                            binding.result.text = "You must consume "+cal+" calories to maintain your weight"
+                        }
+
+                    }else if(binding.fActividad.selectedItem.toString() == "Moderate activity"){
+
+                        var cal = ((655 + (9.6 * binding.weight.text.toString().toDouble()) +
+                                ((1.8 * binding.height.text.toString().toDouble()) - (4.7 * binding.age.text.toString().toDouble()))) * 1.55).roundToInt()
+
+                        if(binding.objetivo.selectedItem.toString() == "Lose weight")
+                        {
+                            binding.result.text = "You must consume less than "+cal+" calories to lose weight"
+
+                        }else if(binding.objetivo.selectedItem.toString() == "Gain weight"){
+
+                            binding.result.text = "You must consume more than "+cal+" calories to gain weight"
+
+                        }
+                        else
+                        {
+                            binding.result.text = "You must consume "+cal+" calories to maintain your weight"
+                        }
+
+                    }
+                    else
+                    {
+                        var cal = ((655 + (9.6 * binding.weight.text.toString().toDouble()) +
+                                ((1.8 * binding.height.text.toString().toDouble()) - (4.7 * binding.age.text.toString().toDouble()))) * 1.725).roundToInt()
+
+                        if(binding.objetivo.selectedItem.toString() == "Lose weight")
+                        {
+                            binding.result.text = "You must consume less than "+cal+" calories to lose weight"
+
+                        }else if(binding.objetivo.selectedItem.toString() == "Gain weight"){
+
+                            binding.result.text = "You must consume more than "+cal+" calories to gain weight"
+
+                        }
+                        else
+                        {
+                            binding.result.text = "You must consume "+cal+" calories to maintain your weight"
+                        }
+
+                    }
                 }
             }
 
